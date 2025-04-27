6dad4880166b5e149179f1fb534c95b5199356c5
Ashutosh Kewat <109691178+Ashu6200@users.noreply.github.com>
2025-04-27 23:57:38 +0530

    first commit

---
+++ src/configs/routes.config/sharedRoutes.ts
@@ -0,0 +1,60 @@
+// src/configs/routes.config/index.ts
+import { lazy } from 'react'
+import authRoute from './authRoute'
+import othersRoute from './othersRoute'
+// import sharedRoutes from './sharedRoutes'
+import type { Routes } from '@/@types/routes'
+
+// Public routes (accessible without authentication)
+export const publicRoutes: Routes = [
+    ...authRoute,
+    // ...sharedRoutes, // Add shared routes here
+]
+
+// Protected routes (require authentication)
+export const SharedRoutes: Routes = [
+    {
+        key: 'homePage',
+        path: '/',
+        component: lazy(() => import('@/views/Home')),
+        authority: [],
+    },
+    {
+        key: 'singleMenuItem',
+        path: '/single-menu-view',
+        component: lazy(() => import('@/views/demo/SingleMenuView')),
+        authority: [],
+    },
+    {
+        key: 'collapseMenu.item1',
+        path: '/collapse-menu-item-view-1',
+        component: lazy(() => import('@/views/demo/CollapseMenuItemView1')),
+        authority: [],
+    },
+    {
+        key: 'collapseMenu.item2',
+        path: '/collapse-menu-item-view-2',
+        component: lazy(() => import('@/views/demo/CollapseMenuItemView2')),
+        authority: [],
+    },
+    {
+        key: 'groupMenu.single',
+        path: '/group-single-menu-item-view',
+        component: lazy(() => import('@/views/demo/GroupSingleMenuItemView')),
+        authority: [],
+    },
+    {
+        key: 'groupMenu.collapse.item1',
+        path: '/group-collapse-menu-item-view-1',
+        component: lazy(() => import('@/views/demo/GroupCollapseMenuItemView1')),
+        authority: [],
+    },
+    {
+        key: 'groupMenu.collapse.item2',
+        path: '/group-collapse-menu-item-view-2',
+        component: lazy(() => import('@/views/demo/GroupCollapseMenuItemView2')),
+        authority: [],
+    },
+    // ...sharedRoutes, // Add shared routes here too
+    ...othersRoute,
+]
---
+++ src/views/Home/index.tsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useRef } from 'react';
+import HeroSection from './components/HeroSection';
+import HomeFAQs from './components/HomeFAQ';
+import ContactForm from './components/ContactForm';
+import InfoSection from './components/InfoSection';
+import FeaturesGrid from './components/FeaturesGrid';
+
+const Home: React.FC = () => {
+	const contactRef = useRef<HTMLDivElement>(null);
+	const aboutRef = useRef<HTMLDivElement>(null);
+	const FqRef = useRef<HTMLDivElement>(null);
+
+	const scrollToSection = (ref: React.RefObject<HTMLElement>) => {
+		if (ref.current) {
+			ref.current.scrollIntoView({ behavior: 'smooth' });
+		}
+	};
+
+
+	useEffect(() => {
+		let lastScrollTop = 0; // Initialize lastScrollTop variable
+
+		const handleScroll = () => {
+			const hcf = document.querySelector(".hcf-profile");
+			const scrollTop =
+				document.documentElement.scrollTop || document.body.scrollTop;
+
+			if (scrollTop > lastScrollTop) {
+				if (hcf) {
+					hcf.classList.add("hcf-profile-fixed");
+				}
+			} else if (scrollTop < lastScrollTop) {
+				if (hcf) {
+					hcf.classList.remove("hcf-profile-fixed");
+				}
+			}
+
+			lastScrollTop = scrollTop;
+		};
+
+
+		// Add scroll event listener
+		window.addEventListener("scroll", handleScroll);
+
+		// Cleanup the event listener on unmount
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+	return (
+		<>
+			<div>
+				<div className="">
+					<HeroSection
+						scrollToSection={scrollToSection}
+						featuresRef={FqRef}
+						contactRef={contactRef}
+						aboutRef={aboutRef}
+					/>
+					{/* <div className='bg-white'>
+						<ClaimLandingSection />
+					</div> */}
+					<div className='!bg-[#eff6ff] relative'>
+						<FeaturesGrid />
+					</div>
+					<div ref={aboutRef} className='!bg-white relative'>
+						<InfoSection />
+					</div>
+					<div ref={FqRef} className='relative bg-white'>
+						<HomeFAQs />
+					</div>
+					<div ref={contactRef} className='bg-white relative' >
+						<ContactForm />
+					</div>
+					{/* <div className='bg-white'>
+						<MainFooter />
+					</div> */}
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Home;
